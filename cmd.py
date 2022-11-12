14b8a91a291cd52cc72820adabd4ca9a9a1afe0c
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-11-12 15:56:05 -0300

    Hello, World debugable

--- cmd.py
+++ cmd.py
@@ -10,13 +10,13 @@ command = inputs[1]
 try:
     if (command == "build"):
         print("[Python CMD]  Project is building.")
-        subprocess.run(["dotnet", "build"])
+        subprocess.run(["dotnet", "build", "src/Goals.sln"])
     elif (command == "run"):
         print("[Python CMD]  Project is running.")
-        subprocess.run(["dotnet", "run", "--project", "src/api"])
+        subprocess.run(["dotnet", "run", "--project", "src/Api"])
     elif (command == "wrun"):
         print("[Python CMD]  Project is running in watch mode.")
-        subprocess.run(["dotnet", "watch", "run", "--project", "src/api"])
+        subprocess.run(["dotnet", "watch", "run", "--project", "src/Api"])
     else:
         print("[Python CMD]  Didn't Match Anything.")
 except KeyboardInterrupt:
